11ed9dd9dc1a14570e6a43433f1505f2b0646f6b
Qian Gong <gongqian.de@gmail.com>
2026-04-16 09:56:40 +0200

    chore(init): configure React app with Vite, TailwindCSS, DaisyUI, and React Router

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    open: true,
+    port: 5173,
+  },
+});
